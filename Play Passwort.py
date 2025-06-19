17c5aba9a4ee710d1068de114b6a505ef0dfc1bb
Isabel Bröder <130468181+isabelbroeder@users.noreply.github.com>
2025-06-19 12:48:22 +0200

    Vergleich Buchstaben Eingegebenes Wort mit Passwort
    Prüfen ob Buchstaben, an der richtigen Position sind oder im Wort enthalten sind aber an der falschen Position sind oder nicht vorkommen. 
    Dafür Listen in Großbuchstaben umgewandelt
    Funktioniert noch nicht, wenn Buchstaben mehrfach im Word enthalten sind

--- Play Passwort.py
+++ Play Passwort.py
@@ -1,15 +1,34 @@
 import numpy as np
 import random
 
-wortliste = ['Apfel', 'Birne', 'Katze', 'Blume', 'Tisch', 'Stuhl', 'Lampe']
-random.choice(wortliste)
-print(random.choice(wortliste))
+#Passwort aus Wortliste zufällig auswählen
+wortliste = ['Apfel', 'Birne', 'Katze', 'Blume', 'Tisch', 'Stuhl', 'Lampe', 'Besen', 'Leine']
+passwort = random.choice(wortliste).upper()
+passwortListe = list(passwort)
+print(f"Das Passwort ist {passwort}")
 
-wort = input("Gib ein Wort ein: ")
-print("Du hast das Wort eingegeben:", wort)
+#Wort eingeben
+eingabe = input("Gib ein Wort ein: ").upper()
+eingabeListe = list(eingabe)
+print("Du hast das Wort eingegeben:", eingabe)
 
-print (wort in wortliste)
-if (wort in wortliste): 
+#Prüfen, ob eingebenes Wort in der Wortliste enthalten ist
+if (eingabe in wortliste): 
     print("Das Wort ist in der Liste!")
 else: 
     (print("Das Wort ist nicht in der Liste!"))
+
+#Buchstaben check
+for i in range (0,len(passwortListe)):
+    print(f"{i}, Buchstabe Eingabe {eingabeListe[i]}, Buchstabe Passwort {passwortListe[i]}")
+    if eingabeListe[i] == passwortListe[i]:
+        print(f"Buchstabe {i + 1} ist grün")
+    elif (eingabeListe[i] not in passwortListe):
+        print(f"Buchstabe {i + 1} ist rot")
+    else:
+        print(f"Buchstabe {i + 1} ist gelb")
+
+
+#Prüfen ob eingebenes Wort gleich dem Passwort ist
+if (eingabe == passwort):
+    print("Du hast das Passwort erraten :)")
