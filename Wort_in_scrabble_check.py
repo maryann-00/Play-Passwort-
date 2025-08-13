f83ce561d9d9eddd848ddd736567b50a69327a52
isabelbroeder <isabel.broeder@gmx.de>
2025-08-13 20:27:15 +0200

    Wörter mit Scrabble Tool überprüfen

--- Wort_in_scrabble_check.py
+++ Wort_in_scrabble_check.py
@@ -1,48 +1,55 @@
-import ctypes
-from multiprocessing import process
+from collections import Counter
+import cv2
+import numpy as np
+import  keyboard
+import  re
 import subprocess
-
+import psutil
 from pywinauto import Application
 import pyautogui
-import pytesseract
 import time
 
 
-#pfad_scrabble = r"C:\Users\isabe\3D Objects\MSc 4\Practical Python Development\projekt_passwort\SDeV-Turnier-Check_250501\SDeV-Check.exe"
-pfad_scrabble = r"SDeV-Turnier-Check_250501/SDeV-Check.exe"
+def scrabble_check(wort: str) -> bool:
 
-proc = subprocess.Popen([pfad_scrabble])
-# Kurze Pause, um das Fenster in den Vordergrund zu bringen
-time.sleep(1)
+    #Scrabble Check öffnen
+    pfad_scrabble = r"Scrabbel Wort Check\SDeV- Check.exe"
+    proc = subprocess.Popen(pfad_scrabble)
+    #Tastatur sperren
+    for i in range(150):
+        keyboard.block_key(i)
+    # Kurze Pause, um das Fenster in den Vordergrund zu bringen
+    time.sleep(1)
 
-# Wort eingeben
-#pyautogui.write("abcdefghi") #ungültiges Wort
-pyautogui.write("fahrender") #gültiges Wort
-time.sleep(1)
+    # Wort eingeben
+    #pyautogui.write("ii") #ungültiges Wort
+    #pyautogui.write("hammer") #gültiges Wort
+    pyautogui.write(wort) #übergebener Parameter
+    #time.sleep(1)
+    pyautogui.press('enter')
+    time.sleep(1)
 
-# Enter drücken
-pyautogui.press('enter')
+    #Verbinden mit Scrabble Wort Check
+    app = Application(backend="uia").connect(process=proc.pid)
+
+    # Hole das Fenster
+    dlg = app.window(title_re=".*Scrabble.*")
+
+    #Position von Fenster 'SDeV Scrabble-Check 01.05.2025' berechnen
+    rect = dlg.rectangle()
+    print("Position (X, Y):", rect.left, rect.top)
+    # Breite und Höhe berechnen
+    width = rect.right - rect.left
+    height = rect.bottom - rect.top
 
 
-#app = Application(backend="uia").connect(title_re=".*")  # Passe den Fenstertitel an
-app = Application(backend="uia").connect(process=proc.pid)
-
-# Hole das Fenster
-dlg = app.window(title_re=".*Scrabble.*")  # Versuche, mit allgemeinem Muster alle Fenster zu finden
-
-#Position von Fenster 'SDeV Scrabble-Check 01.05.2025' berechnen
-rect = dlg.rectangle()
-print("Position (X, Y):", rect.left, rect.top)
-# Breite und Höhe berechnen
-width = rect.right - rect.left
-height = rect.bottom - rect.top
-
-
-screenshot = pyautogui.screenshot(region=(rect.left, rect.top , width, height))
-screenshot_ausschnitt = pyautogui.screenshot(region=(int(1.2* rect.left), 2*rect.top , int(0.7*width), int(0.15 * height)))
-#print(type(screenshot))
-#screenshot.show()
-screenshot_ausschnitt.show()
-#screenshot_ausschnitt.save('screenshot_scrabble_existiert_nicht.jpg')
-
-#text = pytesseract.image_to_string(screenshot)
+    #screenshot = pyautogui.screenshot(region=(rect.left, rect.top , width, height))
+    screenshot_ausschnitt = pyautogui.screenshot(region=(int(1.2* rect.left), 2*rect.top , int(0.7*width), int(0.15 * height)))
+    print(type(screenshot_ausschnitt))
+    #screenshot.show()
+    #screenshot_ausschnitt.show()
+    screenshot_ausschnitt.save('screenshot_scrabble.jpg')
+    for i in range(150):
+        keyboard.unblock_key(i)
+    proc.terminate()
+scrabble_check("hallo")
