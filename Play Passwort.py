61ad93758f752cc66c2617744c51898c5fe7098b
isabelbroeder <isabel.broeder@gmx.de>
2025-08-15 10:38:37 +0200

    Dateien zusammen geführt

--- Play Passwort.py
+++ Play Passwort.py
@@ -6,16 +6,43 @@ from Wort_in_scrabble_check import scrabble_check
 
 pygame.init()
 
+# screen setup
+white = (255, 255, 255)
+black = (0, 0, 0)
+
 screen_width = 800
 screen_height = 600
 
-screen = pygame.display.set_mode((screen_width, screen_height)) #create game window
+screen = pygame.display.set_mode((screen_width, screen_height))  # create game window
+
+turn = 0  # initialize turn variable
+# create matrix
+board = [[" ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " "],
+         [" ", " ", " ", " ", " ", " "]]
+
+
+def draw_board():
+    global turn
+    global board
+    for col in range(0, 5):  # end value excluded
+        for row in range(0, 6):
+            pygame.draw.rect(screen, white, [col * 100, row * 100, 75, 75], 3,
+                             5)  # 3 and 5 at the end rounds rectangles
+
 
 run = True
-while run: #initialize game loop
+while run:  # initialize game loop
+    screen.fill(black)#black
+    draw_board()
+    pygame.display.flip()
 
-    for event in pygame.event.get(): #allows us to iterate over all the events that pygame picks up
-        if event.type == pygame.QUIT: #statement to close pygame window
+
+    for event in pygame.event.get():  # allows us to iterate over all the events that pygame picks up
+        if event.type == pygame.QUIT:  # statement to close pygame window
             run = False
 
 pygame.quit()
@@ -42,10 +69,10 @@ def wort_eingabe():
     print("Du hast das Wort eingegeben:", eingabe)
     return eingabe, eingabe_liste
 
-def richtige_wortlaenge(eingabe):
-    if (len(eingabe) != laenge):
-        print(f"Das Wort muss {laenge} Buchstaben haben")
-    return len(eingabe) == laenge
+def richtige_wortlaenge(eingabe, laenge):
+    if (len(eingabe) != 5):
+        print("Das Wort muss 5 Buchstaben haben")
+    return len(eingabe) == 5
 
 
 def wort_in_wortliste(eingabe, wortliste):
@@ -59,7 +86,7 @@ def wort_in_wortliste(eingabe, wortliste):
 passwort_erraten = False
 while not passwort_erraten:
     eingabe, eingabe_liste = wort_eingabe()
-    if not richtige_wortlaenge(eingabe):
+    if not richtige_wortlaenge(eingabe,laenge):
         continue
 
     #if not wort_in_wortliste(eingabe, wortliste):
