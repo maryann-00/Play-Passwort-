2c1c6ffffd34c7c8e5b96e4a1569c335bb3150b6
isabelbroeder <isabel.broeder@gmx.de>
2025-09-24 15:28:54 +0200

    Dateien umbenannt

--- Play_Passwort.py
+++ Play_Passwort.py
@@ -4,7 +4,7 @@ import re
 import sys
 import time
 import pygame
-from buchstaben_einfarben import buchstaben_einfaerben, ist_wort_erlaubt
+from funktionen_play_passwort import buchstaben_einfaerben, ist_wort_erlaubt
 #from Wort_in_scrabble_check import scrabble_check
 
 
